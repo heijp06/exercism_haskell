cfa4c2429cd6a7b0559bd8541ba4a6fc2d135b70
Peter van der Heijden <heijp06@gmail.com>
2023-10-31 20:21:27 +0100

    Define StateT transformer.

--- bowling/src/Bowling.hs
+++ bowling/src/Bowling.hs
@@ -1,8 +1,32 @@
 module Bowling (score, BowlingError(..)) where
 
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.State.Lazy (StateT, evalStateT)
+
 data BowlingError = IncompleteGame
                   | InvalidRoll { rollIndex :: Int, rollValue :: Int }
   deriving (Eq, Show)
 
+data GameState = GameState { frame :: Int
+                           , points :: Int
+                           , firstRoll :: Bool
+                           , afterSpare :: Bool
+                           , firstAfterStrike :: Bool
+                           , secondAfterStrike :: Bool
+                           }
+
+newGame :: GameState
+newGame = GameState { frame = 1
+                    , points = 0
+                    , firstRoll = True
+                    , afterSpare = False
+                    , firstAfterStrike = False
+                    , secondAfterStrike = False
+                    }
+
 score :: [Int] -> Either BowlingError Int
-score rolls = Right $ sum rolls
+score rolls = evalStateT bar newGame
+
+bar :: StateT GameState (Either BowlingError) Int
+bar = do
+  lift $ Right 42
