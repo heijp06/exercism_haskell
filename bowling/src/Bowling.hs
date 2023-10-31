0e3997c5449aea61d4f3d61752530cff886ffa51
Peter van der Heijden <heijp06@gmail.com>
2023-10-31 21:10:07 +0100

    Some of the tests with spares start succeeding.

--- bowling/src/Bowling.hs
+++ bowling/src/Bowling.hs
@@ -1,32 +1,47 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Bowling (score, BowlingError(..)) where
 
 import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State.Lazy (StateT, evalStateT)
+import Control.Monad.Trans.State.Lazy (StateT, evalStateT, get, put)
 
 data BowlingError = IncompleteGame
                   | InvalidRoll { rollIndex :: Int, rollValue :: Int }
   deriving (Eq, Show)
 
-data GameState = GameState { frame :: Int
+data GameState = GameState { rollsLeft :: [Int]
+                           , frame :: Int
                            , points :: Int
                            , firstRoll :: Bool
+                           , firstRollScore :: Int
                            , afterSpare :: Bool
                            , firstAfterStrike :: Bool
                            , secondAfterStrike :: Bool
                            }
 
-newGame :: GameState
-newGame = GameState { frame = 1
-                    , points = 0
-                    , firstRoll = True
-                    , afterSpare = False
-                    , firstAfterStrike = False
-                    , secondAfterStrike = False
-                    }
+newGame :: [Int] -> GameState
+newGame rolls = GameState { rollsLeft = rolls
+                          , frame = 1
+                          , points = 0
+                          , firstRoll = True
+                          , firstRollScore = 0
+                          , afterSpare = False
+                          , firstAfterStrike = False
+                          , secondAfterStrike = False
+                          }
 
 score :: [Int] -> Either BowlingError Int
-score rolls = evalStateT bar newGame
+score rolls = evalStateT bar (newGame rolls)
 
 bar :: StateT GameState (Either BowlingError) Int
 bar = do
-  lift $ Right 42
+  state@GameState{..} <- get
+  case rollsLeft of
+    [] -> lift $ Right points
+    (x:xs) -> do
+      put (state { rollsLeft = xs
+                 , points = points + if afterSpare then 2*x else x
+                 , firstRoll = not firstRoll
+                 , firstRollScore = if firstRoll then x else 0
+                 , afterSpare = firstRollScore + x == 10 })
+      bar
