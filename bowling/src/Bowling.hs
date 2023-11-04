a67efd9e17f47876f07e80654ed0357053e08d01
Peter van der Heijden <heijp06@gmail.com>
2023-11-04 10:46:21 +0100

    Add peekRoll.

--- bowling/src/Bowling.hs
+++ bowling/src/Bowling.hs
@@ -2,10 +2,10 @@
 
 module Bowling (score, BowlingError(..)) where
 
-import Control.Applicative ((<|>))
-import Control.Monad (guard, replicateM, when)
+import Control.Monad (replicateM, when)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.State.Lazy (StateT, evalStateT, get, put)
+import Prelude hiding (error)
 
 data BowlingError = IncompleteGame
                   | InvalidRoll { rollIndex :: Int, rollValue :: Int }
@@ -17,8 +17,7 @@ data Frame = OneBallThrown Int | OneBonusBall Int | TwoBonusBalls Int | Complete
 data GameState = GameState { rolls :: [Int]
                            , current_roll :: Int
                            , max_roll :: Int
-                           , error :: BowlingError 
-                           }
+                           } deriving Show
 
 type Evaluator = StateT GameState (Either BowlingError)
 
@@ -26,7 +25,6 @@ newGame :: [Int] -> GameState
 newGame rolls = GameState { rolls = rolls
                           , current_roll = 0
                           , max_roll = 0
-                          , error = IncompleteGame 
                           }
 
 score :: [Int] -> Either BowlingError Int
@@ -42,25 +40,17 @@ frame = do
   roll <- getRoll
   case roll of
     10 -> do
-      x <- getRoll
-      y <- getRoll
-      rollBack
-      rollBack
+      x <- peekRoll 0
+      y <- peekRoll 1
       return $ 10 + x + y
     _ -> do
       x <- getRoll
       case roll + x of
         10 -> do
-          y <- getRoll
-          rollBack
+          y <- peekRoll 0
           return $ 10 + y
         _ -> do
           return $ roll + x
-
-rollBack :: Evaluator ()
-rollBack = do
-  state@GameState{..} <- get
-  put state { current_roll = current_roll - 1 }
 
 getRoll :: Evaluator Int
 getRoll = do
@@ -70,3 +60,13 @@ getRoll = do
   when (roll < 0 || roll > 10) $ lift $ Left $ InvalidRoll current_roll roll
   put state { current_roll = current_roll + 1, max_roll = max current_roll max_roll }
   return $ rolls !! current_roll
+
+peekRoll :: Int -> Evaluator Int
+peekRoll offset = do
+  state@GameState{..} <- get
+  let index = current_roll + offset
+  when (index >= length rolls) $ lift $ Left IncompleteGame
+  let roll = rolls !! index
+  when (roll < 0 || roll > 10) $ lift $ Left $ InvalidRoll current_roll index
+  put state { max_roll = max index max_roll }
+  return roll
